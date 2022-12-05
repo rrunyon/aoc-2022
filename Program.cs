c00ab8a518dc33d24c33507f8a95020825ea754c
Rick Runyon <rick.runyon@dutchie.com>
2022-12-05 11:09:36 -0500

    Day 5 Part 2

--- Program.cs
+++ Program.cs
@@ -17,3 +17,5 @@ System.Console.WriteLine(Solutions.FourTwo.Output());
 
 System.Console.WriteLine("Solution for 5-1:");
 System.Console.WriteLine(Solutions.FiveOne.Output());
+System.Console.WriteLine("Solution for 5-2:");
+System.Console.WriteLine(Solutions.FiveTwo.Output());
