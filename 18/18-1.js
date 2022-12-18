cc84c6f34624c0eee5b1fdd1c7c515f8b519bd22
Rick Runyon <rick.runyon@dutchie.com>
2022-12-18 17:22:22 -0500

    Day 18 Part 1

---
+++ 18/18-1.js
@@ -0,0 +1,38 @@
+import * as fs from 'fs';
+
+const dirs = [
+  [0, 0, 1],
+  [0, 0, -1],
+  [0, 1, 0],
+  [0, -1, 0],
+  [1, 0, 0],
+  [-1, 0, 0],
+];
+
+function measureDropletSurfaceArea() {
+  let input = fs.readFileSync('./18/input.txt', { encoding: 'utf8', flag: 'r' }).split('\n');
+  let droplet = new Set;
+  for (let line of input) {
+    droplet.add(line);
+  }
+
+  let exposedFaces = 0;
+  for (let line of input) {
+    let [x, y, z] = line.split(',').map(i => parseInt(i));
+
+    for (let dir of dirs) {
+      let newX = x + dir[0];
+      let newY = y + dir[1];
+      let newZ = z + dir[2];
+      let plot = `${newX},${newY},${newZ}`;
+
+      if (!droplet.has(plot)) {
+        exposedFaces++;
+      }
+    }
+  }
+
+  return exposedFaces;
+}
+
+console.log(measureDropletSurfaceArea());
